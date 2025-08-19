0640e2ed6d2274b95255fa5e1fa84f4b86af4517
Shantanu-Nagwekar-01 <shantanunagwekar12@gmail.com>
2025-08-20 02:35:15 +0530

    add Output dir and Main and pattern0 java files

---
+++ Basics/Output/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //output
+        //System is a class
+        System.out.println("Hello, Friend!"); //print is a function
+        System.out.print("Elliot Alderson\n");// backslash n '\n' is used to go to next line
+        //print : System.out.print("String"); --> print String but no new line, cursor will be on same line
+        //println is used or \n inside quotes
+        System.out.println("Hello, Friend!\nElliot Alderson"); 
+    }
+}
---
+++ Basics/Output/pattern0.java
@@ -0,0 +1,15 @@
+public class pattern0 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
+//Output:
+/*
+*
+**
+***
+****
+ */
