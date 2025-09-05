0bbba5bf7dbb490c8cd8c096d00bcff7124f318e
Shantanu_Nagwekar <shantanunagwekar12@gmail.com>
2025-09-05 23:52:36 +0530

    add Input directory and input.java exploring scanner class to take input from console

---
+++ Basics/Input/input.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // sc is object of scanner class;
+        // System.out.print("Enter first name: ");
+        // String first_name = sc.next(); // will take only first token not full line
+        System.out.print("\nEnter Full name: ");
+        String full_name = sc.nextLine(); // will take full line as input
+        System.out.println("\nFull Name is: " + full_name);
+        System.out.print("\nEnter age: ");
+        int age = sc.nextInt(); // takes next integer input
+        System.out.println(full_name + "'s age is " + age);
+        System.out.print("Enter your height in cm: ");
+        float height = sc.nextFloat(); // takes next float input
+        System.out.println(full_name + "'s height is " + height + "cm.");
+
+    }
+}
